04cb99ff799fa065c7fd22b33cd266694f89e69f
Muhammad Abdullah <moabdullah588@gmail.com>
2022-05-19 00:18:52 +0200

    change contact page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:obelix_agency/presentaion/screen/contact_us/contact_us.dart';
 import 'package:sizer/sizer.dart';
 import 'app_routes.dart';
 import 'bussiness_logic/navigation_bottom/nav_bar_cubit.dart';
@@ -22,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Sizer(builder: (context, orientation, deviceType) {
-    /*  return FutureBuilder(
+      return FutureBuilder(
           future: Init.instance.initialize(),
           builder: (context, AsyncSnapshot snapshot) {
             // Show splash screen while waiting for app resources to load:
@@ -31,19 +30,18 @@ class MyApp extends StatelessWidget {
                 debugShowCheckedModeBanner: false,
                 home: Splash(),
               );
-            } else {*/
+            } else {
               // Loading is done, return the app:
               return MaterialApp(
                 debugShowCheckedModeBanner: false,
-                // theme: ThemeData.dark(),
-                home: ContactUs() /* BlocProvider(
+                theme: ThemeData.dark(),
+                home: BlocProvider(
                   create: (BuildContext context) => NavBarCubit(),
                   child: const NavigationBottom(),
-                ),*/
+                ),
               );
-           // }
+            }
           });
-    }
-   // );
+    });
   }
-//}
+}
